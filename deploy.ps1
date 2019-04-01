16650f157bde4b07287f94cb1f981e30a654c2a4
adeelaleem <40723709+adeelaleem@users.noreply.github.com>
2019-04-01 07:07:57 +0500

    NodeJS Web App ARM Template in JSON

---
+++ deploy.ps1
@@ -0,0 +1,7 @@
+﻿Connect-AzureRmAccount
+
+New-AzureRmResourceGroup -Name RG-Node-App -Location "Canada Central"
+
+New-AzureRmResourceGroupDeployment -Name NodeAppDeployment -ResourceGroupName RG-Node-App `
+  -TemplateFile D:\NodeJSAPp\template.json `
+  -TemplateParameterFile D:\NodeJSAPp\parameters.json
